c142db39e0e5b07ba641b3dac54ca2909d20ade2
kritsanapr <kritsana.pr.41@gmail.com>
2022-10-21 20:23:25 +0700

    Add prisma and migrate to postgres

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { AuthModule } from './auth/auth.module';
 import { Module } from '@nestjs/common';
 import { UsersModule } from './users/users.module';
 import { BookmarkModule } from './bookmark/bookmark.module';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [AuthModule, UsersModule, BookmarkModule],
+  imports: [AuthModule, UsersModule, BookmarkModule, PrismaModule],
   controllers: [],
   providers: [],
 })
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,12 +1,27 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from './../prisma/prisma.service';
+import { AuthDto } from './dto';
+import * as argon from 'argon2';
 
-@Injectable({})
+@Injectable()
 export class AuthService {
-  signin() {
-    return { msg: 'I have signin' };
+  constructor(private prisma: PrismaService) {}
+
+  async signup(dto: AuthDto) {
+    // generate the password hash
+    const hash = await argon.hash(dto.password);
+    // save the new user in the db
+    const user = await this.prisma.user.create({
+      data: {
+        email: dto.email,
+        hash,
+      },
+    });
+    // return the saved user
+    return user;
   }
 
-  signup() {
-    return { msg: 'I have signup' };
+  signin() {
+    return { msg: 'I have signin' };
   }
 }
